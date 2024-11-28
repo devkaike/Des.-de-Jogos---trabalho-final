fcc134b471935f3d98f7ed364947af2fc1ae0279
Alexandre-CA <alexandr220404@gmail.com.br>
2024-11-27 23:57:50 -0300

    video 05

--- scenes/options.gd
+++ scenes/options.gd
@@ -1,44 +1,56 @@
 extends VBoxContainer
-
-@export var waepons: HBoxContainer
-var OptionSlot = preload('res://scenes/option_slot.tscn')
-
-func _ready() -> void:
+ 
+@export var weapons : HBoxContainer
+@export var passive_items : HBoxContainer
+var OptionSlot = preload("res://scenes/option_slot.tscn")
+ 
+func _ready():
 	hide()
-
-func get_available_waepons():
-	var weapon_resource = []
-	for weapon in waepons.get_children():
-		if weapon.weapon != null:
-			weapon_resource.append(weapon.weapon)
-	return weapon_resource
-	
+ 
 func close_option():
 	hide()
 	get_tree().paused = false
-
-func show_options():
-	#var option_slot = OptionSlot.instantiate()
-	#add_child(option_slot)
-	#
-	#show()
-	#get_tree().paused = true
-	var weapons_available = get_available_waepons()
-	if weapons_available.size() == 0:
+ 
+func get_available_resource_in(items)-> Array[Item]:
+	var resources : Array[Item] = []
+	for item in items.get_children():
+		if item.item != null:
+			resources.append(item.item)
+	return resources
+ 
+func add_option(item) -> int:
+	if item.is_upgrabled():
+		var option_slot = OptionSlot.instantiate()
+		option_slot.item = item
+		add_child(option_slot)
+		return 1
+	return 0
+ 
+func show_option():
+	var weapons_available = get_available_resource_in(weapons)
+	var passive_item_available = get_available_resource_in(passive_items)
+	if weapons_available.size() == 0 and passive_item_available.size() == 0:
 		return
+ 
 	for slot in get_children():
 		slot.queue_free()
-	
+ 
 	var option_size = 0
 	for weapon in weapons_available:
-		if weapon.is_upgrabled():
+		option_size += add_option(weapon)
+ 
+		if weapon.max_level_reached() and weapon.item_needed in passive_item_available:
 			var option_slot = OptionSlot.instantiate()
-			option_slot.weapon = weapon
+			option_slot.item = weapon
 			add_child(option_slot)
 			option_size += 1
-	
+ 
+	for passive_item in passive_item_available:
+		option_size += add_option(passive_item)
+ 
 	if option_size == 0:
 		return
-	
+ 
 	show()
 	get_tree().paused = true
+ 
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,26 @@
 extends CharacterBody2D
  
-var speed : float = 150
-var health : float = 100.0 :
+var health : float = 100 :
 	set(value):
-		health = value
+		health = max(value,0)
 		%Health.value = value
-var nearest_enemy: CharacterBody2D
-var nearest_enemy_distance: float = INF
+
+var armor:float = 0
+var might:float = 1.5
+var area: float = 50
+
+var max_health : float = 100.0 :
+	set(value):
+		max_health = value
+		%Health.max_value = value
+		
+var movement_speed:float = 150
+var recovery: float = 0
+var magnet: float = 0:
+	set(value):
+		magnet = value
+		%Magnet.shape.radius = 50 + value
+var growth: float = 1
 
 var XP : int = 0:
 	set(value):
@@ -18,17 +32,23 @@ var level : int = 1:
 	set(value):
 		level = value
 		%Level.text ="Lv " + str(value)
-		%Options.show_options()
+		%Options.show_option()
 		if level >= 2:
 			%XP.max_value = 20
 		elif level >= 7:
 			%XP.max_value = 40
+			
+			
+var nearest_enemy: CharacterBody2D
+var nearest_enemy_distance: float = 150 + area
+
 func _physics_process(delta):
 	if is_instance_valid(nearest_enemy):
 		nearest_enemy_distance = nearest_enemy.separation
 	else:
-		nearest_enemy_distance = INF
-	velocity = Input.get_vector("left","right","up","down") * speed
+		nearest_enemy_distance = 150 + area
+		nearest_enemy = null
+	velocity = Input.get_vector("left","right","up","down") * movement_speed
 	move_and_collide(velocity * delta)
 	#if velocity == Vector2.ZERO:
 		#$AnimationPlayer.play("idle")
@@ -40,23 +60,24 @@ func _physics_process(delta):
 	#elif velocity.x > 0:
 		#$Sprite2D.flip_h = false
 	check_XP()
+	health += recovery * delta
 
 	
 func take_damage(amount):
-	health -= amount
-	print(amount)
+	health -= max(amount - armor,0)
  
  
 func _on_self_damage_body_entered(body):
 	take_damage(body.damage)
  
 func gain_XP(amount):
-	XP += amount
-	total_XP += amount
+	XP += amount * growth
+	total_XP += amount * growth
 	
 func check_XP():
 	if XP > %XP.max_value:
 		XP -= %XP.max_value
+		%XP.max_value = %XP.max_value * 1.5
 		level += 1
 
 func _on_timer_timeout() -> void:
